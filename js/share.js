3bd06e79d3dfbac5a190e2122bf951dc5bf7a799
soyunKang <rkdthdbs99@naver.com>
2023-02-21 22:59:01 +0900

    tset

---
+++ js/share.js
@@ -0,0 +1,33 @@
+const url = 'https://twelvelovetype.netlify.app/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '유형별 쓰줍러 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'resultimg/result' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
