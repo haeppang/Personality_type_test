603e15e1c97d334b82f528d59de9a70fe1ad302d
soyunKang <rkdthdbs99@naver.com>
2023-03-01 23:47:48 +0900

    update

--- js/share.js
+++ js/share.js
@@ -4,9 +4,10 @@ function setShare() {
     var resultImg = document.querySelector('#resultImg');
     var resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '나의 유형별 쓰줍러 결과';
-    const shareDes = infoList[resultAlt].name;
+    const shareDes = infoList[resultAlt].name + infoList[resultAlt].desc;
     const shareImage ='./shareimg/result' + resultAlt + '.png';
     const shareURL = url;
+    console.log(shareImage);
 
     Kakao.Link.sendDefault({
         objectType: 'feed',
