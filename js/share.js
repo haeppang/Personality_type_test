bcb1bf4806fd2e9e18768181f16ddd4c221a22cc
soyunKang <rkdthdbs99@naver.com>
2023-03-02 22:17:30 +0900

    update

--- js/share.js
+++ js/share.js
@@ -4,7 +4,7 @@ function setShare() {
     var resultImg = document.querySelector('#resultImg');
     var resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '나의 유형별 쓰줍러 결과';
-    const shareDes = infoList[resultAlt].name + infoList[resultAlt].desc;
+    const shareDes = infoList[resultAlt].name;
     const shareImage =url + 'resultimg/result' + resultAlt + '.jpg';
     const shareURL = url + 'page/result-'+ resultAlt + '.html';
 
